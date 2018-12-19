59b1031fdcb9280a70bd4d3c885a09a85be95c1d
Stanislav Semashko <StanislavSemashko5@gmail.com>
2018-12-19 19:35:10 +0100

    All tests passing

--- car.cpp
+++ car.cpp
@@ -7,20 +7,21 @@ struct CarImplimintation
   enum Color color;
   double fill_level;
   double acceleration_rate;
-  int speed;
+  double speed;
   bool is_available = true;
   double maximum_acceleration;
   double minimum_accelaration;
+  int max_speed;
 
 };
 
 
-struct CarImplimintation aixam1{AIXAM, RED, 16.0, 0.0, 0, true, 1, -8};
-struct CarImplimintation fiat1{FIAT_MULTIPLA, GREEN, 65.0, 0.0, 0, true,2.26, -8};
-struct CarImplimintation fiat2{FIAT_MULTIPLA, BLUE, 65.0, 0.0, 0, true, 2.26, -8};
-struct CarImplimintation fiat3{FIAT_MULTIPLA, ORANGE ,65.0, 0.0, 0, true, 2.26, -8};
-struct CarImplimintation jeep1{JEEP, SILVER, 80.0, 0.0, 0, true, 3.14, -8};
-struct CarImplimintation jeep2{JEEP, BLACK, 80.0, 0.0, 0, true, 3.14, -8};
+struct CarImplimintation aixam1{AIXAM, RED, 16.0, 0.0, 0, true, 1, -8, 45};
+struct CarImplimintation fiat1{FIAT_MULTIPLA, GREEN, 65.0, 0.0, 0, true,2.26, -8, 170};
+struct CarImplimintation fiat2{FIAT_MULTIPLA, BLUE, 65.0, 0.0, 0, true, 2.26, -8, 170};
+struct CarImplimintation fiat3{FIAT_MULTIPLA, ORANGE ,65.0, 0.0, 0, true, 2.26, -8, 170};
+struct CarImplimintation jeep1{JEEP, SILVER, 80.0, 0.0, 0, true, 3.14, -8, 196};
+struct CarImplimintation jeep2{JEEP, BLACK, 80.0, 0.0, 0, true, 3.14, -8, 196};
 
 Car car_park[6]={&aixam1, &fiat1, & fiat2, &fiat3, &jeep1, &jeep2};
 
@@ -54,7 +55,11 @@ double get_acceleration_rate(Car car)
 }
 int get_speed(Car car)
 {
-  return car->speed;
+  if (car->speed - (int)car->speed >= 0.5 && car->speed +1 <= car->max_speed)
+  {
+    return (int)car->speed+1;
+  }
+  return (int)car->speed;
 }
 void init()
 {
@@ -82,5 +87,14 @@ void set_acceleration_rate(Car car, double acceleration)
 }
 void accelerate(Car car)
 {
-  car->speed = car->acceleration_rate * 4;
+  double speed = car->acceleration_rate + 3.6;
+  if (car->speed + speed > car->max_speed)
+  {
+    car->speed = car->max_speed;
+  }
+  else
+  {
+      car->speed += car->acceleration_rate * 3.6;
+  }
+
 }
